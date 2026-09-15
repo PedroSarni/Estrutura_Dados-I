cd5477dcacd6ac487eebf6701c1dcfc1bd0fa9ba
Pedro Sarni <pedroabsarni@gmail.com>
2026-09-14 21:53:32 -0300

    Melhoria na Documentação interna

--- projeto_1_ED1/prj_1.c
+++ projeto_1_ED1/prj_1.c
@@ -1,71 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void exibicao(int *arr, int n){
-    for (int i=0; i< n; i++){
+// função que printa os elementos do vetor
+void exibicao(int *arr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
         printf("%d ", arr[i]);
     }
     printf("\n");
 }
 
 
-void bin_rec(int arr[], int l, int r, int target){
-    if(l>r){
-        //caso não encontre o valor target
-        printf("NAO\n");
-        return ;
+// Função 1: Inversão da ordem do vetor de entrada
+void inverter_arr(int arr[], int n)
+{
+    int *arr_temp = malloc(n * sizeof(int)); // aloca um vetor temporario
+    // for para copiar o vetor original para um vetor temporário
+    for (int i = 0; i < n; i++)
+    {
+        arr_temp[i] = arr[i];
     }
-    int meio=l+(r-l)/2;
-    if(arr[meio]==target){
-        printf("SIM\n");
-        return;
-    }
-    if(arr[meio]<target){
-        return bin_rec(arr, meio+1, r, target);
-    }
-    if(arr[meio]>target){
-        return bin_rec(arr, l, meio-1, target);
-    }
-}
-void bin_int(int arr[], int l, int r, int target){
-    while(l<=r){
-        int meio=l+(r-l)/2;
-
-        if(arr[meio]==target){
-            printf("SIM\n");
-            return;
-        }
-
-        if (arr[meio]<target){
-            l=meio+1;
-        }
-
-        if(arr[meio]>target){
-            r=meio-1;
-        }
-    }
-    //caso finalize o loop, significa que o valor target não foi encontrado
-    printf("NAO\n");
-    return;
-}
-
-void inverter_arr(int arr[] , int n){
-    int *arr_temp=malloc(n*sizeof(int));//aloca um vetor temporario
-    //for para copiar o vetor original para um vetor temporário
-    for(int i = 0; i < n ; i++){
-            arr_temp[i] = arr[i];
-        }
-    //ponteiro no inicio do vetor
-    int l=0;
-    //ponteiro no final do vetor
-    int r=n-1;
-    //variavel temporaria para facilitar a troca
+    // ponteiro no inicio do vetor
+    int l = 0;
+    // ponteiro no final do vetor
+    int r = n - 1;
+    // variavel temporaria para facilitar a troca
     int temp;
-    while(l<r){
-        temp=arr_temp[l];
-        arr_temp[l]=arr_temp[r];
-        arr_temp[r]=temp;
-        //convergência dos ponteiros para o meio do vetor
+    while (l < r)
+    {
+        temp = arr_temp[l];
+        arr_temp[l] = arr_temp[r];
+        arr_temp[r] = temp;
+        // convergência dos ponteiros para o meio do vetor
         l++;
         r--;
     }
@@ -73,37 +40,87 @@ void inverter_arr(int arr[] , int n){
     return;
 }
 
-void busca_sequencial(int arr[], int n, int target){
-    for (int i=0; i< n; i++){
-        if (arr[i]==target){
+// Função 2: Busca Sequencial por iteratividade
+void busca_sequencial(int arr[], int n, int target)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == target)
+        {
             printf("SIM\n");
             return;
         }
     }
-    //se finzalizou o loop e não achou é pq o target nn consta no vetor
+    // se finzalizou o loop e não achou é pq o target nn consta no vetor
     printf("NAO\n");
     return;
 }
 
-int main(){
-    //Criação do vetor com n entradas;
-    int n;
-    scanf("%d", &n);
-    int arr[n];
-    int target;
+// Função 3: Busca Binaria por iteratividade
+void bin_int(int arr[], int l, int r, int target)
+{
+    while (l <= r)
+    {
+        int meio = l + (r - l) / 2;
 
-    for (int i=0; i<n; i++){
-        scanf("%d", &arr[i]);
+        if (arr[meio] == target)
+        {
+            printf("SIM\n");
+            return;
+        }
+
+        if (arr[meio] < target)
+        {
+            l = meio + 1;
+        }
+
+        if (arr[meio] > target)
+        {
+            r = meio - 1;
+        }
     }
+    // caso finalize o loop, significa que o valor target não foi encontrado
+    printf("NAO\n");
+    return;
+}
 
+// Função 4: Busca Binária por recursão
+void bin_rec(int arr[], int l, int r, int target)
+{
+    if (l > r)
+    {
+        // caso não encontre o valor target
+        printf("NAO\n");
+        return;
+    }
+    int meio = l + (r - l) / 2;
+    if (arr[meio] == target)
+    {
+        printf("SIM\n");
+        return;
+    }
+    if (arr[meio] < target)
+    {
+        return bin_rec(arr, meio + 1, r, target);
+    }
+    if (arr[meio] > target)
+    {
+        return bin_rec(arr, l, meio - 1, target);
+    }
+}
 
-    int escolha=0;
-    while(escolha!=5){
+void menu_opcoes(int *arr, int n)
+{
+    // De acordo com a opção lida, chama-se a funcao para escolher qual dos algoritmos serão utilizados
+    int target;
+    int escolha = 0;
+    while (escolha != 5)
+    {
         scanf("%d", &escolha);
         switch (escolha)
         {
         case 1:
-            inverter_arr(arr,n);
+            inverter_arr(arr, n);
             break;
         case 2:
             scanf("%d", &target);
@@ -111,12 +128,12 @@ int main(){
             break;
         case 3:
             scanf("%d", &target);
-            bin_int(arr, 0, n-1, target);
+            bin_int(arr, 0, n - 1, target);
             break;
 
         case 4:
             scanf("%d", &target);
-            bin_rec(arr, 0, n-1, target);
+            bin_rec(arr, 0, n - 1, target);
             break;
 
         case 5:
@@ -127,6 +144,22 @@ int main(){
             break;
         }
     }
+}
+
+int main()
+{
+    // Criação do vetor com n entradas;
+    int n;
+    scanf("%d", &n);
+    int *arr = malloc(n * sizeof(int));
+
+    // entrada dos  n elementos no vetor
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    menu_opcoes(arr, n);
 
     return 0;
 }
